7d4d95bba55b357b10f9b666cadd020e44e3d76f
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-15 14:30:00 +0100

    Stampa con for e while

---
+++ script.js
@@ -0,0 +1,17 @@
+let listaSpesa = ["avocado", "uova", "pane", "latte", "succo", "patatine", "sale", "olio"];
+
+console.log("STAMPA FOR");
+
+for (let i = 0; i < listaSpesa.length; i++) {
+   console.log(listaSpesa[i]);
+
+}
+
+console.log("STAMPA WHILE");
+let j = 0;
+while(j < listaSpesa.length){
+
+    console.log(listaSpesa[j]);
+    j++
+
+}
