d334e90b45306a4b557aeca61c23498f296cae68
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-15 15:52:50 +0100

    Pulsante aggiunta

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let listaSpesa = ["avocado", "uova", "pane", "latte", "succo", "patatine", "sale", "olio"];
+let listaSpesa = ["avocado", "uova", "pane", "latte", "succo", "patatine", "sale", "sale", "olio"];
 
+// IL SALE NON PIACE ALL'HTML?
 console.log("STAMPA FOR");
 
 for (let i = 0; i < listaSpesa.length; i++) {
@@ -17,7 +18,18 @@ while(j < listaSpesa.length){
 }
 
 let elenco = document.querySelector(".elencoPuntato");
+let pulsanteX = '<button type="button" class="btn-close" aria-label="Close"></button>'; //  "+pulsanteX+"
 
 for (let i = 0; i < listaSpesa.length; i++) {
-    elenco.innerHTML += "<li>"+listaSpesa[i]+"</li>";
+    elenco.innerHTML += "<li><h4>"+listaSpesa[i]+"</h4></li>";
  }
+
+ let aggButton = document.getElementById("aggiungi");
+
+ aggButton.addEventListener('click', function(){
+
+    let aggiunta = prompt("Quale elemento vuoi aggiungere?");
+    elenco.innerHTML += "<li><h4>"+aggiunta+"</h4></li>";
+
+ })
+
